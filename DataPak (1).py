b2de6b21601e617c1049a87a486920e849e1df77
RP1990Data <rahulspund1@gmail.com>
2025-11-22 12:36:38 +0530

    Add files via upload

---
+++ DataPak (1).py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def CA(r):
+    return 3.14*r*r
+
+
+# In[2]:
+
+
+def sqrt(num):
+    return num**0.5
+
+
+# In[3]:
+
+
+def cbrt(num):
+    return num**(1/3)
+
+
+# In[4]:
+
+
+def mylen(data):
+    c=0
+    for i in data:
+        c+=1
+    return c
+
+
+# In[ ]:
+
+
+
+
